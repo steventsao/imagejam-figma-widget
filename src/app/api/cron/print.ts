e2199ed372295c8bb9c5911801757dfde6098e69
Steven Tsao <steventsao@me.com>
2023-08-24 09:53:54 -0700

    implement

---
+++ src/app/api/cron/print.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  return NextResponse.json(
+    { title: "cron" },
+    {
+      status: 200,
+    }
+  );
+}
