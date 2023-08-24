70113ecc9b71481142136cbc648e05bd217ea4cb
Steven Tsao <steventsao@me.com>
2023-08-24 09:10:26 -0700

    every minute

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -1,6 +1,8 @@
-// add a route
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+// Store about 1000 words with definitions
+// Since they're small, try inmemory storage
+// Maybe a vector store to offer similarity search
 export default function (request: VercelRequest, response: VercelResponse) {
   const { name = "World" } = request.query;
   response.send(`Hello ${name}!`);
