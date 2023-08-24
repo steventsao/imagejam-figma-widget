d2474f136b7a0266b3dc4f9da054d2eec74bba32
Steven Tsao <steventsao@me.com>
2023-08-24 12:05:54 -0700

    words api

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -1,9 +1,14 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { sql } from "@vercel/postgres";
 
 // Store about 1000 words with definitions
 // Since they're small, try inmemory storage
 // Maybe a vector store to offer similarity search
-export default function (request: VercelRequest, response: VercelResponse) {
+export default async function (
+  request: VercelRequest,
+  response: VercelResponse
+) {
   const { name = "World" } = request.query;
-  response.send(`Hello ${name}!`);
+  const { rows, fields } = await sql`SELECT * from words`;
+  response.json({ rows, fields });
 }
