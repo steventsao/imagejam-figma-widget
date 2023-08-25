66ec50a205f6e194bca9ac2d9c2b48f327a286cc
Steven Tsao <steventsao@me.com>
2023-08-24 22:18:16 -0700

    components folder

---
+++ src/components/LinksSection.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+export default function LinksSection() {
+  return (
+    <>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <Link
+          href="/tools/golf-terms"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Golf Terms{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Got to know your words
+          </p>
+        </Link>
+        <Link
+          href="/tools/random-golf-club-generator"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Club Generator{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Wheel of misfortune
+          </p>
+        </Link>
+      </div>
+    </>
+  );
+}
