5af2518ed0db948aba93e913c074f3db3de8cf3d
Steven Tsao <steventsao@me.com>
2023-08-29 22:26:38 -0700

    s3

---
+++ pages/api/aws.ts
@@ -0,0 +1,26 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import aws from "aws-sdk";
+import crypto from "crypto";
+
+aws.config.update({
+  accessKeyId: process.env.AWS_S3_ACCESS_ID,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  region: "us-west-1", // e.g., 'us-west-1'
+});
+
+const s3 = new aws.S3();
+
+export default async function (
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  const params = {
+    Bucket: "bogeybot",
+    Key: crypto.randomUUID(),
+    Body: request.body,
+  };
+  await s3.putObject(params, (err, data) => {
+    console.log(data);
+    response.send(data);
+  });
+}
