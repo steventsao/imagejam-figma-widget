acca2a466d902b8af88b71997f5a20ccae5f69cb
Steven Tsao <steventsao@me.com>
2023-09-03 10:49:29 -0700

    add types

--- src/components/MyDropzone.tsx
+++ src/components/MyDropzone.tsx
@@ -1,65 +1,66 @@
 "use client";
-import { Group, Text, useMantineTheme, rem } from '@mantine/core';
-import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react';
-import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from '@mantine/dropzone';
+import { Group, Text, useMantineTheme, rem } from "@mantine/core";
+import { IconUpload, IconPhoto, IconX } from "@tabler/icons-react";
+import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 
-function fileToDataURI(file, callback) {
-    const reader = new FileReader();
+function fileToDataURI(file: File, callback: Function) {
+  const reader = new FileReader();
 
-    reader.onload = function(event) {
-        callback(event.target.result);
-    };
+  reader.onload = function (event) {
+    if (!event.target) {
+      throw new Error("No target");
+    }
+    callback(event.target.result);
+  };
 
-    reader.onerror = function(error) {
-        console.error('Error reading file:', error);
-    };
+  reader.onerror = function (error) {
+    console.error("Error reading file:", error);
+  };
 
-    reader.readAsDataURL(file);
-
+  reader.readAsDataURL(file);
 }
+// @ts-ignore
 const postFile = async (files) => {
-	// TODO need to accept jpeg too
-	const file = files[0]
+  // TODO need to accept jpeg too
+  const file = files[0];
 
-	if (!file) {
-		throw new Error("Must attach file");
-	}
-	console.log('uploading file', file)
-	// TODO nested bS
-	await fileToDataURI(file, async (result) => {
-		console.log(result);
-			const promise = await fetch("/api/pose", {method:'POST', headers:{"Content-Type":"image/png"}, body:result})
-	const data = await promise.body;
-	console.log(data, promise);
-	return data;
-	})
-
-
-
-}
-export default function MyDropzone(props:Partial<DropzoneProps>) {
-
-	return (<Dropzone
+  if (!file) {
+    throw new Error("Must attach file");
+  }
+  console.log("uploading file", file);
+  // TODO nested bS
+  // @ts-ignore
+  await fileToDataURI(file, async (result) => {
+    console.log(result);
+    const promise = await fetch("/api/pose", {
+      method: "POST",
+      headers: { "Content-Type": "image/png" },
+      body: result,
+    });
+    const data = await promise.body;
+    console.log(data, promise);
+    return data;
+  });
+};
+export default function MyDropzone(props: Partial<DropzoneProps>) {
+  return (
+    <Dropzone
       onDrop={postFile}
-      onReject={(files) => console.log('rejected files', files)}
+      onReject={(files) => console.log("rejected files", files)}
       maxSize={3 * 1024 ** 2}
       accept={IMAGE_MIME_TYPE}
       {...props}
     >
-      <Group position="center" spacing="xl" style={{ minHeight: rem(220), pointerEvents: 'none' }}>
+      <Group
+        position="center"
+        spacing="xl"
+        style={{ minHeight: rem(220), pointerEvents: "none" }}
+      >
         <Dropzone.Accept>
-          <IconUpload
-            size="3.2rem"
-            stroke={1.5}
-            color={4}
-          />
+          <IconUpload size="3.2rem" stroke={1.5} color="4" />
         </Dropzone.Accept>
         <Dropzone.Reject>
-          <IconX
-            size="3.2rem"
-            stroke={1.5}
-            color={4}
-          />
+          <IconX size="3.2rem" stroke={1.5} color="4" />
         </Dropzone.Reject>
         <Dropzone.Idle>
           <IconPhoto size="3.2rem" stroke={1.5} />
@@ -74,5 +75,6 @@ export default function MyDropzone(props:Partial<DropzoneProps>) {
           </Text>
         </div>
       </Group>
-    </Dropzone>)
+    </Dropzone>
+  );
 }
