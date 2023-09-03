5b9d3de9694457816a6ddc30dc76d8ca7494545c
Steven Tsao <steventsao@me.com>
2023-09-03 14:32:25 -0700

    gotta fix the flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,12 +29,16 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <MyDropzone />
       <Space h="xl" />
-      {/* @ts-ignore */}
-      {swingImages.map((url, i) => (
-        <Card shadow="sm" padding="xl" key={i}>
-          <Image src={url} height={300} alt={`golf swing ${i}`} />
-        </Card>
-      ))}
+      <section>
+        <div className="flex flex-col justify-start">
+          {/* @ts-ignore */}
+          {swingImages.map((url, i) => (
+            <Card shadow="sm" padding="xl" key={i}>
+              <Image src={url} width={150} alt={`golf swing ${i}`} />
+            </Card>
+          ))}
+        </div>
+      </section>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
