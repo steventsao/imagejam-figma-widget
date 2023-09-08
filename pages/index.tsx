156630f569c40219ea102834fed0609aac9c3ac5
Steven Tsao <steventsao@me.com>
2023-09-08 09:36:15 -0700

    add source

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 type SwingItem = {
   image_url: string;
 };
+
+// https://stackoverflow.com/questions/76725399/nextjs-how-to-fix-getserversideprops-is-not-supported-in-app
 export const getServerSideProps = async () => {
   const supabase = createClientComponentClient();
   const promise = await supabase.from("swing-public").select("image_url");
@@ -17,7 +19,5 @@ export default function Home({ swingImages }: { swingImages: SwingItem[] }) {
   // TODO select * from "Swing" inner join "Prediction" on "Prediction"."swingId" = "Swing".id where "Swing"."blobId" is not null and "Prediction"."url" is not null
 
   // @ts-ignore
-  console.log(swingImages, "hi");
-
   return <MainSection swingImages={swingImages} />;
 }
