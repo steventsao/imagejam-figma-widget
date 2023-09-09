ee262071e72c2550351f1d18a93bc66f381ee27c
Steven Tsao <steventsao@me.com>
2023-09-09 00:32:14 -0700

    Add swing route

---
+++ pages/swing/[swingId].tsx
@@ -0,0 +1,41 @@
+import { GetServerSidePropsContext } from "next";
+import { Image } from "@mantine/core";
+import { sql } from "@vercel/postgres";
+
+type SwingProps = {
+  swing: {
+    id: number;
+    createdAt: Date;
+    image_url?: string;
+    blobId?: string;
+    userId?: number;
+    // The types for `user`, `s3`, and `predictions` would depend on their respective structures.
+    // Replace `any` with the appropriate types.
+    user?: any;
+    s3?: any;
+    predictions?: any[];
+  };
+};
+// Get swingId from path /swing/[swingId]
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const swingId = Array.isArray(context.params?.swingId)
+    ? context.params?.swingId[0]
+    : context.params?.swingId;
+  // TODO needs to be predictionId instead of `swingId` so it cannot be enumerated
+  const result =
+    await sql`select "PredictionImage".url as image_url from "PredictionImage" INNER JOIN "Prediction" on "Prediction".id = "PredictionImage"."predictionId" where "PredictionImage"."predictionId" = ${swingId}`;
+
+  console.log(result.rows[0], "swing");
+  return { props: { swing: result.rows[0] } };
+};
+
+export default function Swing({ swing }: SwingProps) {
+  return (
+    <>
+      <div>hi {swing.id}</div>
+      <Image src={swing.image_url} alt={`golf swing ${swing.id}`} />
+    </>
+  );
+}
