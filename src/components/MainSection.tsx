f2cc4e65710743cf7b3e1c162f7328296b018c5c
Steven Tsao <steventsao@me.com>
2023-09-10 13:07:02 -0700

    less margin

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -62,7 +62,7 @@ export default function MainSection({
       <Space h="xl" />
       <MyDropzone />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="bottom-0 left-0 flex h-48 w-full items-end justify-center">
+        <div className="bottom-0 flex w-full">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
