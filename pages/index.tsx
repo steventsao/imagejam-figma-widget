d9dfc7716c67c110851005dbc729ebdef2d5d321
Steven Tsao <steventsao@me.com>
2023-09-10 22:56:58 -0700

    am i using layout right

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,140 @@
-import MainSection from "@/components/MainSection";
-import { sql } from "@vercel/postgres";
+import { GetServerSidePropsContext } from "next";
 import { Analytics } from "@vercel/analytics/react";
-import { Container } from "@mantine/core";
-import aws from "aws-sdk";
-import { SwingItem } from "@/lib/types";
-import "../styles/globals.css";
+import Layout from "@/components/layout";
+import {
+  AppShell,
+  Container,
+  Group,
+  Image,
+  Card,
+  Button,
+  Text,
+  Slider,
+  useMantineTheme,
+} from "@mantine/core";
+import { useState } from "react";
+// useRouter import
 
-// TODO https://github.com/steventsao/bogeybot/issues/8
-// use supabase realtime database later
-
-const IMAGEKIT_URL = "https://ik.imagekit.io/cirnjtkq1/";
-aws.config.update({
-  accessKeyId: process.env.AWS_S3_ACCESS_ID,
-  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-  region: "us-west-1", // e.g., 'us-west-1'
-});
-
-const s3 = new aws.S3();
-
-// get public URL of s3 objects
-
-// https://stackoverflow.com/questions/76725399/nextjs-how-to-fix-getserversideprops-is-not-supported-in-app
-export const getServerSideProps = async () => {
-  // TODO Converge supabase and postgres into one payload
-  // https://vercel.com/steventsao-pro/bogeybot/stores/postgres/store_21MWJgp02zT6wYOZ/data
-  //   const payload =
-  // await sql`select "Prediction"."url" from "Swing" inner join "Prediction" on "Prediction"."swingId" = "Swing".id where "Swing"."blobId" is not null and "Prediction"."url" is not null`;
-  const payload =
-    await sql`WITH swing_prediction as (select "Swing".id as swing_id, "Prediction"."url" as prediction_url from "Swing" inner join "Prediction" on "Prediction"."swingId" = "Swing".id where "Swing"."blobId" is not null and "Prediction"."url" is not null)
-    select s3.key as s3_key, swing_prediction.prediction_url as prediction_image_url from s3 inner join swing_prediction on s3."swingId" = swing_prediction.swing_id order by s3."createdAt" desc`;
-  console.log(payload.rows, "payload");
-  const swingImages = payload.rows.map((item) => {
-    const imageUrl = IMAGEKIT_URL + item.s3_key;
-    return { image_url: imageUrl, ...item };
-  });
-  return {
-    props: {
-      swingImages: swingImages || [],
-    },
+type SwingProps = {
+  swing?: {
+    id: number;
+    createdAt: Date;
+    image_url?: string;
+    blobId?: string;
+    userId?: number;
+    // The types for `user`, `s3`, and `predictions` would depend on their respective structures.
+    // Replace `any` with the appropriate types.
+    user?: any;
+    s3?: any;
+    predictions?: any[];
   };
+  swingFrames: string[];
 };
-// https://supabase.com/dashboard/project/tkbhdbhsnikrpsutyyxk/settings/api?
-export default function Home({ swingImages }: { swingImages: SwingItem[] }) {
-  // @ts-ignore
+const max_frame = 750;
+const aws_base = "https://bogeybot.s3.us-west-1.amazonaws.com";
+const imagekit_base = "https://ik.imagekit.io/cirnjtkq1/tr:q-25";
+// https://ik.imagekit.io/cirnjtkq1/steven-test-swing/frame_0700.png
+const getFrameUrl = (frame: number, urlBase = imagekit_base): string => {
+  // for frame 1, return frame_0001.png, for frame 750, return frame_0750.png
+  let frameString = frame.toString();
+  let frameLength = frameString.length;
+  while (frameLength < 4) {
+    frameString = "0" + frameString;
+    frameLength++;
+  }
+  return `${urlBase}/slomo/frame_${frameString}.png`;
+};
+const getFrameUrls = (maxFrame: number): string[] => {
+  let currentFrame = 1;
+  const urls: string[] = [];
+  while (currentFrame <= maxFrame) {
+    urls.push(getFrameUrl(currentFrame));
+    currentFrame++;
+  }
+  return urls;
+};
+// Get swingId from path /swing/[swingId]
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const urls = getFrameUrls(max_frame);
+  return { props: { swingFrames: urls } };
+};
+
+export default function Home({ swingFrames }: SwingProps) {
+  const [frame, setFrame] = useState(1);
+
   return (
-    <>
+    <Layout>
       <Container p="xs">
-        <MainSection swingImages={swingImages} />;
-        <Analytics />
+        <Card shadow="sm" padding="lg" radius="md">
+          <Card.Section>
+            <Image
+              maw={800}
+              mx="auto"
+              radius="md"
+              src={getFrameUrl(frame)}
+              alt={`golf swing ${frame}`}
+            />
+          </Card.Section>
+          <Group position="apart" mt="md" mb="xs">
+            <Button
+              onClick={() => {
+                setFrame(frame - 10);
+              }}
+            >
+              Back 10
+            </Button>
+            <Button
+              onClick={() => {
+                setFrame(frame - 1);
+              }}
+            >
+              Back
+            </Button>
+            <Button
+              onClick={() => {
+                setFrame(frame + 1);
+              }}
+            >
+              Next
+            </Button>
+            <Button
+              onClick={() => {
+                setFrame(frame + 10);
+              }}
+            >
+              Next 10
+            </Button>
+          </Group>
+          <Text>
+            Frame {frame} of {max_frame}
+          </Text>
+          <Slider
+            value={Math.floor((frame / max_frame) * 100)}
+            onChange={(currentPercent: number) => {
+              const frame = Math.floor(max_frame * (currentPercent / 100));
+              setFrame(frame);
+              // const currentPercent = Math.floor((frame / max_frame) * 100);
+            }}
+            marks={[
+              { value: 20, label: "20%" },
+              { value: 50, label: "50%" },
+              { value: 80, label: "80%" },
+            ]}
+          />
+        </Card>
+        {/* <Card>
+        <Card.Section>
+          <Image
+            src={swing.image_url}
+            width={300}
+            alt={`golf swing ${swing.id}`}
+          />
+        </Card.Section>
+      </Card> */}
       </Container>
-    </>
+      <Analytics />
+    </Layout>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,24 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import {
+  AppShell,
+  Navbar,
+  Container,
+  Group,
+  Image,
+  Card,
+  Button,
+  Text,
+  Slider,
+  useMantineTheme,
+} from "@mantine/core";
+import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "bogeybot",
-  description: "free stuff for golfers",
-};
+// export const metadata: Metadata = {
+//   title: "bogeybot",
+//   description: "free stuff for golfers",
+// };
 
 // Duplicated from /app/layout.tsx
 export default function RootLayout({
@@ -14,9 +26,31 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppShell
+          styles={{
+            main: {
+              // background: theme.colors.gray[8],
+            },
+          }}
+          navbar={
+            <Navbar
+              p="md"
+              hiddenBreakpoint="sm"
+              hidden={!opened}
+              width={{ sm: 200, lg: 300 }}
+            >
+              <Text>bogeybot</Text>
+            </Navbar>
+          }
+        >
+          {children}
+        </AppShell>
+      </body>
     </html>
   );
 }
