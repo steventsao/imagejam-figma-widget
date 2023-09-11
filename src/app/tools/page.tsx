3ade001eac796a80fcaa2f59911257755892df84
Steven Tsao <steventsao@me.com>
2023-09-11 00:24:22 -0700

    undo

--- src/app/tools/page.tsx
+++
@@ -1,5 +0,0 @@
-// https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes
-// TODO add type and use RSC https://nextjs.org/docs/pages/building-your-application/routing/dynamic-routes
-export default async function Page(props: any) {
-  return <p>not found, please return to home page</p>;
-}
