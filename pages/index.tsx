f430b3b4be23e268fb76979010b0afd1da06505d
Steven Tsao <steventsao@me.com>
2023-09-12 00:04:07 -0700

    looks good for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { GetServerSidePropsContext } from "next";
 import Layout from "@/components/pagesLayout";
-import { Container, Group, Image, Button, Text, Slider } from "@mantine/core";
-import { useState } from "react";
+import { Container, Image } from "@mantine/core";
+import { useState, Suspense } from "react";
+import FramesControls from "@/components/FramesControls";
 import { sql } from "@vercel/postgres";
 // import "@/styles/globals.css";
 
@@ -76,61 +77,9 @@ export default function Home({ swingFrames, uploads }: ViewerProps) {
           src={getFrameUrl(frame)}
           alt={`golf swing ${frame}`}
         />
-        <Group position="apart" mt="md" mb="xs">
-          <Button
-            // secondary
-            compact={true}
-            variant="outline"
-            onClick={() => {
-              setFrame(frame - 10);
-            }}
-          >
-            Back 10
-          </Button>
-          <Button
-            compact={true}
-            variant="outline"
-            onClick={() => {
-              setFrame(frame - 1);
-            }}
-          >
-            Back
-          </Button>
-          <Button
-            compact={true}
-            variant="outline"
-            onClick={() => {
-              setFrame(frame + 1);
-            }}
-          >
-            Next
-          </Button>
-          <Button
-            compact={true}
-            variant="outline"
-            onClick={() => {
-              setFrame(frame + 10);
-            }}
-          >
-            Next 10
-          </Button>
-        </Group>
-        <Text>
-          Frame {frame} of {max_frame}
-        </Text>
-        <Slider
-          value={Math.floor((frame / max_frame) * 100)}
-          onChange={(currentPercent: number) => {
-            const frame = Math.floor(max_frame * (currentPercent / 100));
-            setFrame(frame);
-            // const currentPercent = Math.floor((frame / max_frame) * 100);
-          }}
-          marks={[
-            { value: 20, label: "20%" },
-            { value: 50, label: "50%" },
-            { value: 80, label: "80%" },
-          ]}
-        />
+        <Suspense fallback={<div>Loading...</div>}>
+          <FramesControls frame={frame} setFrame={setFrame} />
+        </Suspense>
         {/* <Card>
         <Card.Section>
           <Image
