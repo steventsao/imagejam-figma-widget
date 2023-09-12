3808cf0d541d37130fa86af4a054036464d3e1b1
Steven Tsao <steventsao@me.com>
2023-09-12 15:47:47 -0700

    cors

--- pages/api/s3-frames.ts
+++ pages/api/s3-frames.ts
@@ -3,19 +3,38 @@ import Replicate from "replicate";
 import { sql } from "@vercel/postgres";
 import { PrismaClient } from "@prisma/client";
 import { SupabaseClient } from "@supabase/supabase-js";
-
+// allow amzn lambda
+const allowCors =
+  (fn: any) => async (req: VercelRequest, res: VercelResponse) => {
+    res.setHeader("Access-Control-Allow-Credentials", true + "");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    if (req.method === "OPTIONS") {
+      res.status(200).end();
+      return;
+    }
+    return await fn(req, res);
+  };
 const prisma = new PrismaClient();
 const supabase = new SupabaseClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL || "",
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
 );
 
-export default async function (
-  request: VercelRequest,
-  response: VercelResponse
-) {
+const handler = async (request: VercelRequest, response: VercelResponse) => {
   console.log("received webhook...");
   const data = await request.body;
   console.log("YEEEE", data, request.query);
   response.status(200).send("ok");
-}
+};
+
+export default allowCors(handler);
