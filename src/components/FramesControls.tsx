c1d8d4a96fd91092e21228df1ff3632aeb0c1004
Steven Tsao <steventsao@me.com>
2023-09-13 17:01:04 -0700

    simpler button set

--- src/components/FramesControls.tsx
+++ src/components/FramesControls.tsx
@@ -57,18 +57,27 @@ export default function FramesControls({
   // }, [bookmarks]);
   return (
     <Stack>
+      {share ? (
+        <Group position="apart"></Group>
+      ) : (
+        <Text>
+          Frame {frame} of {maxFrame}
+        </Text>
+      )}
+      <Slider
+        className="mt-3"
+        min={1}
+        max={maxFrame}
+        value={frame}
+        onChange={(frame: number) => {
+          setFrameQuery(frame);
+          // const currentPercent = Math.floor((frame / maxFrame) * 100);
+        }}
+        step={5}
+        // TODO why does optimistics updates fuck up on transition such that it leaves the last swing bookmark present?
+        marks={bookmarks}
+      />
       <Group position="apart" mt="md" mb="xs">
-        <Button
-          // secondary
-          // className="text-black-500"
-          compact={true}
-          variant="outline"
-          onClick={() => {
-            setFrameQuery(frame - 10);
-          }}
-        >
-          {"<<"} 10 frames
-        </Button>
         <Button
           compact={true}
           variant="outline"
@@ -87,80 +96,47 @@ export default function FramesControls({
         >
           1 frame {">"}
         </Button>
+
         <Button
-          compact={true}
           variant="outline"
-          onClick={() => {
-            setFrameQuery(frame + 10);
+          onClick={async () => {
+            // Optimistic update alongside hard refresh? https://www.joshwcomeau.com/nextjs/refreshing-server-side-props/
+            setOptimisticBookmarks((b) => [
+              ...b,
+              { value: frame, label: "impact" },
+            ]);
+
+            const result = await fetch("/api/bookmarks", {
+              method: "POST",
+              body: JSON.stringify({
+                // Destructure to avoid sending the same thing back
+                bookmarks: [{ value: frame, label: "impact" }],
+                swingId: router.query.swingId,
+              }),
+            }).then((success) => {
+              // REFRESH?
+              // I want to localize the changes instead of the whole page
+              // https://www.joshwcomeau.com/nextjs/refreshing-server-side-props/
+              router.replace(router.asPath);
+            });
+            console.log("POSTED bookmarks:", result);
           }}
         >
-          10 frames {">>"}
+          Bookmark frame {frame} as impact frame
         </Button>
+        <CopyButton value={shareUrl} timeout={2000}>
+          {({ copied, copy }) => (
+            <Group className="hover:cursor-pointer" onClick={copy}>
+              <ActionIcon color={copied ? "teal" : "gray"}>
+                {copied ? <IconCheck size="1rem" /> : <IconCopy size="1rem" />}
+              </ActionIcon>
+              <Text className="ml">
+                Share frame {frame} of {maxFrame}
+              </Text>
+            </Group>
+          )}
+        </CopyButton>
       </Group>
-      {share ? (
-        <Group position="apart">
-          <CopyButton value={shareUrl} timeout={2000}>
-            {({ copied, copy }) => (
-              <Group className="hover:cursor-pointer" onClick={copy}>
-                <ActionIcon color={copied ? "teal" : "gray"}>
-                  {copied ? (
-                    <IconCheck size="1rem" />
-                  ) : (
-                    <IconCopy size="1rem" />
-                  )}
-                </ActionIcon>
-                <Text className="ml">
-                  Share frame {frame} of {maxFrame}
-                </Text>
-              </Group>
-            )}
-          </CopyButton>
-          <Button
-            variant="outline"
-            onClick={async () => {
-              // Optimistic update alongside hard refresh? https://www.joshwcomeau.com/nextjs/refreshing-server-side-props/
-              setOptimisticBookmarks((b) => [
-                ...b,
-                { value: frame, label: "impact" },
-              ]);
-
-              const result = await fetch("/api/bookmarks", {
-                method: "POST",
-                body: JSON.stringify({
-                  // Destructure to avoid sending the same thing back
-                  bookmarks: [{ value: frame, label: "impact" }],
-                  swingId: router.query.swingId,
-                }),
-              }).then((success) => {
-                // REFRESH?
-                // I want to localize the changes instead of the whole page
-                // https://www.joshwcomeau.com/nextjs/refreshing-server-side-props/
-                router.replace(router.asPath);
-              });
-              console.log("POSTED bookmarks:", result);
-            }}
-          >
-            Bookmark frame {frame} as impact frame
-          </Button>
-        </Group>
-      ) : (
-        <Text>
-          Frame {frame} of {maxFrame}
-        </Text>
-      )}
-
-      <Slider
-        min={1}
-        max={maxFrame}
-        value={frame}
-        onChange={(frame: number) => {
-          setFrameQuery(frame);
-          // const currentPercent = Math.floor((frame / maxFrame) * 100);
-        }}
-        step={5}
-        // TODO why does optimistics updates fuck up on transition such that it leaves the last swing bookmark present?
-        marks={bookmarks}
-      />
     </Stack>
   );
 }
