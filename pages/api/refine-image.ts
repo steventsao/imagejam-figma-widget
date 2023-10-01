e7f8c103d91797b84ec2446f9c0c6fbed66769e8
Steven Tsao <steventsao@me.com>
2023-09-30 21:26:00 -0700

    Add refine

--- pages/api/refine-image.ts
+++ pages/api/refine-image.ts
@@ -1,27 +1,6 @@
 import Replicate from "replicate";
 import aws from "aws-sdk";
-import crypto from "crypto";
 import { NextApiRequest, NextApiResponse } from "next";
-import Cors from "cors";
-const cors = Cors({
-  methods: ["POST", "GET", "HEAD"],
-});
-function runMiddleware(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  fn: Function
-) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
-
 aws.config.update({
   accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_ID,
   secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY,
@@ -32,24 +11,23 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN as string,
 });
 
-const s3 = new aws.S3();
 
 export default async function (
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  await runMiddleware(request, response, cors);
   // image/octet-stream
   const requestType = request.headers["content-type"];
-  const url = request.body;
+  const { url, text}  = request.body;
   console.log(url);
 
   try {
     const output = await replicate.run(
-      "rmokady/clip_prefix_caption:9a34a6339872a03f45236f114321fb51fc7aa8269d38ae0ce5334969981e4cd8",
+        "stability-ai/sdxl:af1a68a271597604546c09c64aabcd7782c114a63539a4a8d14d1eeda5630c33",,
       {
         input: {
           image: url,
+          prompt:text
           // TODO remove this model so it's only pose later
         },
       }
